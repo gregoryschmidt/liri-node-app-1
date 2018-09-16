89e930d5ae83ef65e265f4301a44f93101dfb46c
Michael Cumings <michael.cumings@gmail.com>
2018-09-16 00:22:28 -0500

    still broken

--- liri.js
+++ liri.js
@@ -2,12 +2,22 @@ var request = require("request");
 var fs = require("fs");
 require("dotenv").config();
 var moment = require('moment');
+var keys = require("./keys")
+var Spotify = require('node-spotify-api');
 
 // Parse out the task indicated
 var task = process.argv[2];
 
 // Parse out the thing the user is searching for
 var input = process.argv.slice(3).join(" ");
+
+var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+  });
+
+console.log(keys.spotify.id);
+console.log(keys.spotify.secret);
 
 function concert(input) {
     var url = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
@@ -42,16 +52,99 @@ function concert(input) {
     })
 };
 
+function spotifyts(input) {
+    if (!input) {
+        input = "The Sign"
+    }
+    spotify.search({ type: 'track', query: input}, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+      
+        for (i=0; i < data.tracks.items.length; i++) {
+        // data.tracks.items.forEach(function(trk) {
+            var display = [
+                "\nArtist: " + data.tracks.items[i].artists[0].name,
+                "Song Name: " + data.tracks.items[i].name,
+                "Spotify Link: " + data.tracks.items[i].preview_url,
+                "Album: " + data.tracks.items[i].album.name,
+            ].join('\n');
+     
+            console.log(display);
+            // console.log(self.divider);
+    
+            fs.appendFile('log.txt', display + self.divider, function (err) {
+                if (err) throw err;
+                process.exit();
+               
+            })
+        }; 
+      console.log(data); 
+      });
+}
+
+function moviethis(input) {
+    if (!input) {
+        input = "Mr. Nobody";
+    }
+    var url = "http://www.omdbapi.com/?apikey=trilogy&t=" + input;
+    var self = this;
+
+    request(url, function (err, res, body){
+        if (err) throw err;
+
+        var data = JSON.parse(body);
+
+        // data.forEach(function(movie) {
+            // console.log(element);
+        var rtscore = 0;    
+        for (i=0; i < data.Ratings.length; i++){
+            if (data.Ratings[i].Source = "Rotten Tomatoes") {
+                rtscore = data.Ratings[i].Value
+            }
+        }
+
+        var display = [
+            // * Title of the movie.
+            "\nTitle: " + data.Title,
+            // * Year the movie came out.
+            "Year: " + data.Year,
+            // * IMDB Rating of the movie.
+            "IMDB Rating: " + data.imdbRating,
+            // * Rotten Tomatoes Rating of the movie.
+            "Rotten Tomatoes Rating: " + rtscore,
+            // * Country where the movie was produced.
+            "Country: " + data.Country,
+            // * Language of the movie.
+            "Language: " + data.Language,
+            // * Plot of the movie.
+            "Plot: " + data.Plot,
+            // * Actors in the movie.
+            "Actors: " + data.Actors,
+        ].join('\n');
+ 
+        console.log(display);
+        // console.log(self.divider);
+
+        fs.appendFile('log.txt', display + self.divider, function (err) {
+            if (err) throw err;
+            process.exit();
+           
+        })
+    }); 
+
+}
+
 
 switch (task) {
     case `concert-this`:
         concert(input);
         break;
     case `spotify-this-song`:
-        day = "Monday";
+        spotifyts(input);
         break;
     case `movie-this`:
-        day = `movie-this`;
+        moviethis(input);
         break;
     case `do-what-it-says`:
         day = `do-what-it-says`;
